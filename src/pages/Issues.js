8a411e4060ca87ad05883242101d35bb3f5fa203
Nekeroo <m.grattardpro@gmail.com>
2022-11-25 11:30:26 +0100

    try

--- src/pages/Issues.js
+++ src/pages/Issues.js
@@ -3,38 +3,38 @@ import Fuse from "fuse.js";
 import { useParams } from "react-router-dom";
 
 function Issues() {
-    const [issues, setIssues] = useState([])
+    // const [issues, setIssues] = useState([])
     
-    let {id} = useParams()
-    /* allows us to perform side effects in your components */
-    useEffect(() => {
-        if (id) fetch('https://framagit.org/api/v4/projects/' +  id  + '/issues?membership=true', {
-            method: 'get',
-            headers: new Headers({
-                'PRIVATE-TOKEN' : 'glpat-auESVMAxA27xgNFpu11w',
-                'Accept' : 'application/json'
-            })
-        })
-            .then((response) => response.json())
-            .then((data) => setIssues(data))
-    });
+    // let {id} = useParams()
+    // /* allows us to perform side effects in your components */
+    // useEffect(() => {
+    //     if (id) fetch('https://framagit.org/api/v4/projects/' +  id  + '/issues?membership=true', {
+    //         method: 'get',
+    //         headers: new Headers({
+    //             'PRIVATE-TOKEN' : 'glpat-auESVMAxA27xgNFpu11w',
+    //             'Accept' : 'application/json'
+    //         })
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => setIssues(data))
+    // });
 
-    const fuse = new Fuse(issues);
+    // const fuse = new Fuse(issues);
     
-    return (
+    // return (
     
-        //Affichage des Issuses par rapport au projet sélectionné.
-        <div className='issues'>
-            <h1 className="title">Issues</h1>
-            <h3 className="littleTitle">Number of issues : {fuse.getIndex().size}</h3>
-                {Array.isArray(issues) && issues.map(item => (
-                <div>
-                    <a href={item.web_url}>{item.title}</a>
-                    <br/>
-                </div>
-                ))}
-        </div>
-    )
+    //     //Affichage des Issuses par rapport au projet sélectionné.
+    //     <div className='issues'>
+    //         <h1 className="title">Issues</h1>
+    //         <h3 className="littleTitle">Number of issues : {fuse.getIndex().size}</h3>
+    //             {Array.isArray(issues) && issues.map(item => (
+    //             <div>
+    //                 <a href={item.web_url}>{item.title}</a>
+    //                 <br/>
+    //             </div>
+    //             ))}
+    //     </div>
+    // )
 }
 
 export default Issues;
